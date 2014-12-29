de451d1867938f68dd8a5dfeaffebb4d8eba887a
Jefffrey <notjefff@gmail.com>
2014-12-29 18:41:21 +0100

    Fixed removal of object files in the clean makefile rule

--- makefile
+++ makefile
@@ -1,10 +1,10 @@
 CC				:= g++
 RM				:= rm
 SUITE_EXE 		:= suite
-SUITE_CPP_FILES := $(wildcard test/*.cpp) $(wildcard test/inom/*.cpp)
-SUITE_OBJ_FILES := $(SUITE_CPP_FILES:.cpp=.o)
-LD_FLAGS 		:=
-CC_FLAGS 		:= -std=c++11 -O2
+SUITE_CPP_FILES	:= $(wildcard test/*.cpp) $(wildcard test/inom/*.cpp)
+SUITE_OBJ_FILES	:= $(SUITE_CPP_FILES:.cpp=.o)
+LD_FLAGS		:=
+CC_FLAGS		:= -std=c++11 -O2
 
 suite: $(SUITE_OBJ_FILES)
 	$(CC) $(LD_FLAGS) -o $@ $^
@@ -14,12 +14,12 @@ remake:
 	make
 
 run:
-	make suite
-	./suite
+	make $(SUITE_EXE)
+	./$(SUITE_EXE)
 
 clean:
-	rm -f suite
-	rm -rf 
+	rm -f $(SUITE_EXE)
+	find . -name "*.o" -type f -delete
 
 test/%.o: test/%.cpp
 	$(CC) $(CC_FLAGS) -c -o $@ $<
--- test/inom/util.cpp
+++ test/inom/util.cpp
@@ -1,19 +1,19 @@
 #include "../../ext/catch.hpp"
 #include "../../include/inom/util.hpp"
 
-TEST_CASE("inom::detail::abs") {
+TEST_CASE("inom::detail::constexpr_abs") {
 
     SECTION("it returns 0 on 0") {
-        REQUIRE(inom::detail::abs(0) == 0);
+        REQUIRE(inom::detail::constexpr_abs(0) == 0);
     }
 
     SECTION("it returns the absolute value") {
-        REQUIRE(inom::detail::abs(1) == 1);
-        REQUIRE(inom::detail::abs(2) == 2);
-        REQUIRE(inom::detail::abs(1024) == 1024);
-        REQUIRE(inom::detail::abs(-1) == 1);
-        REQUIRE(inom::detail::abs(-2) == 2);
-        REQUIRE(inom::detail::abs(-1024) == 1024);
+        REQUIRE(inom::detail::constexpr_abs(1) == 1);
+        REQUIRE(inom::detail::constexpr_abs(2) == 2);
+        REQUIRE(inom::detail::constexpr_abs(1024) == 1024);
+        REQUIRE(inom::detail::constexpr_abs(-1) == 1);
+        REQUIRE(inom::detail::constexpr_abs(-2) == 2);
+        REQUIRE(inom::detail::constexpr_abs(-1024) == 1024);
     }
 
 }
