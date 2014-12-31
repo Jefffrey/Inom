9c75c1cbb4ec92e4549eb0c7f37a81ab6d9844d8
Jefffrey <notjefff@gmail.com>
2014-12-31 11:23:11 +0100

    Added type traits (issue #6)
    Adds `includes` which checks if an element is in a range,
    `includes_positives` which checks if any positive number is in the
    range, `includes_negatives` which checks if any negative number is in
    the range.
    
    All three type traits work on generic "ranges", that for this library
    means that `lbound`, `data_t` and `rbound` are compile-time variables
    defining respectively: the left/lower bound of the range, with type
    `data_t`, the type of integer this range class represents, the
    right/upper bound of the range, with type `data_t`.
    
    Adds related tests.

---
+++ include/inom/type_traits.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <type_traits>
+#include "aliases.hpp"
+#include "integer.hpp"
+
+namespace inom {
+
+template<class Range, typename Range::data_t I>
+struct includes {
+    static constexpr bool value = (I >= Range::lbound && I <= Range::rbound);
+};
+
+template<class Range>
+struct includes_positives {
+    static constexpr bool value = (Range::rbound > 0);
+};
+
+template<class Range>
+struct includes_negatives {
+    static constexpr bool value = (Range::lbound < 0);
+};
+
+} // namespace inom
---
+++ test/inom/type_traits.cpp
@@ -0,0 +1,35 @@
+#include "../../ext/catch.hpp"
+#include "../../include/inom/type_traits.hpp"
+
+TEST_CASE("inom::includes") {
+
+    SECTION("it returns true if the specified element is in the range") {
+        REQUIRE((inom::includes<inom::integer<0, 23>, 0>::value) == true);
+        REQUIRE((inom::includes<inom::integer<0, 23>, -1>::value) == false);
+        REQUIRE((inom::includes<inom::integer<-6, 23>, -2>::value) == true);
+        REQUIRE((inom::includes<inom::integer<-39, -2>, 0>::value) == false);
+    }
+
+}
+
+TEST_CASE("inom::includes_positives") {
+
+    SECTION("it returns true if the range includes positive numbers") {
+        REQUIRE((inom::includes_positives<inom::integer<0, 23>>::value) == true);
+        REQUIRE((inom::includes_positives<inom::integer<-30, 0>>::value) == false);
+        REQUIRE((inom::includes_positives<inom::integer<-6, 23>>::value) == true);
+        REQUIRE((inom::includes_positives<inom::integer<-39, -2>>::value) == false);
+    }
+
+}
+
+TEST_CASE("inom::includes_negatives") {
+
+    SECTION("it returns true if the range includes negative numners") {
+        REQUIRE((inom::includes_negatives<inom::integer<0, 23>>::value) == false);
+        REQUIRE((inom::includes_negatives<inom::integer<-32, 0>>::value) == true);
+        REQUIRE((inom::includes_negatives<inom::integer<-6, 23>>::value) == true);
+        REQUIRE((inom::includes_negatives<inom::integer<3, 38>>::value) == false);
+    }
+
+}
